edb4644817cd90f56622883851904d312de88285
NielsPilgaard <niels.pilgaard@hotmail.com>
2019-04-27 21:00:48 +0200

    Name changes.

--- StudentAdministrationWebReact/ClientApp/src/components/NavMenu.js
+++ StudentAdministrationWebReact/ClientApp/src/components/NavMenu.js
@@ -8,7 +8,7 @@ export default props => (
   <Navbar inverse fixedTop fluid collapseOnSelect>
     <Navbar.Header>
       <Navbar.Brand>
-        <Link to={'/'}>StudentAdministrationWeb</Link>
+        <Link to={'/'}>StudentAdministrationWebReact</Link>
       </Navbar.Brand>
       <Navbar.Toggle />
     </Navbar.Header>
